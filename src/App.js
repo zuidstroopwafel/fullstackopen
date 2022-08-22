996be8f071d68263b9ee494aee387f6dddf7e40a
Emanuele123 <emanuelec@match4markets.com>
2022-08-22 13:32:06 +0200

    part1-02

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
 
  
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
-
 export default App
